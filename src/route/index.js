19ac74f7240143365d00f6d6e7147e1c0fa586a6
user <user@user-MacBook-Air.local>
2023-03-13 20:58:44 +0200

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,35 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Andriy',
+    lastname: 'Zeleniy',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ per month',
+  address: 'Ukraine, Ternopil',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'Andriy7733@gmail',
+      href: 'mailto:Andriy7733@gmail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,10 +52,220 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
   })
 })
 
 // ================================================================
 
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: '10',
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: '10',
+          isTop: true,
+        },
+        {
+          name: 'VS Code & NPM',
+          point: '8',
+          isTop: false,
+        },
+        {
+          name: 'Git & Terminal',
+          point: '7',
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'CrossFIT',
+          isMain: true,
+        },
+        {
+          name: 'Football',
+          isMain: false,
+        },
+        {
+          name: 'Winter skiing',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+// ================================================================
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      education: [
+        {
+          name: 'Оксфордський університет (Велика Британія)',
+          isEnd: true,
+        },
+        {
+          name: 'Каліфорнійський технологічний інститут (США)',
+          isEnd: false,
+        },
+        {
+          name: 'Гарвардський університет (США)',
+          isEnd: true,
+        },
+        {
+          name: 'Стенфордський університет (США)',
+          isEnd: false,
+        },
+        {
+          name: 'Кембриджський університет (Велика Британія)',
+          isEnd: true,
+        },
+      ],
+      certificates: [
+        {
+          name: 'JavaScript certificat ',
+          id: '5324',
+        },
+        {
+          name: 'CSS certificat',
+          id: '344546',
+        },
+        {
+          name: 'HTML certificat',
+          id: '34534',
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'http://it-brains.com.ua',
+          },
+
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'http://resume.com.ua',
+              about: 'We use resume to find jobs',
+              stackAmount: '3',
+              awardAmount: '2',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Nodejs',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Нагорода за виконання проекту',
+                },
+                {
+                  name: 'Award for the project',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
